10f44019603566b227d1f224b7c1f5dd6edfecee
HolmesBryant <148395713+HolmesBryant@users.noreply.github.com>
2024-01-18 16:03:28 -0600

    adding code highlighting

--- src/wijit-code-v1.js
+++ src/wijit-code-v1.js
@@ -17,7 +17,7 @@ export default class WijitCode extends HTMLElement {
 	#inline = false;
 	#tabsize = 2;
 	#firstPaint = true;
-	observer;
+	abortController = new AbortController();
 	static observedAttributes = ['inline', 'tabsize'];
 
 	/**
@@ -26,6 +26,25 @@ export default class WijitCode extends HTMLElement {
 	 */
 	constructor() {
 		super();
+		this.attachShadow({mode:'open'});
+		this.shadowRoot.innerHTML = `
+			<style>
+				:host {
+					--tabsize: ${this.tabsize};
+					display: inline-block;
+					vertical-align: middle;
+				}
+
+				pre { tab-size: var(--tabsize); }
+
+				.inline {
+					margin: 0;
+					white-space: nowrap;
+				}
+			</style>
+
+			<pre><code><slot></slot></code></pre>
+		`;
 	}
 
 	/**
@@ -34,43 +53,23 @@ export default class WijitCode extends HTMLElement {
 	 * - Adjusts indentation to match surrounding text.
 	 */
 	connectedCallback() {
-		const content = this.resetSpaces(this);
-		this.mutationConfig = { childList: true, subtree: true };
-		this.observer = new MutationObserver ((mutationList, observer) => {
-			this.observer.disconnect();
-			const content = this.resetSpaces(this);
-			console.log(content)
-			// this.wrapCode(this, content);
-			this.observer.observe(this, this.mutationConfig);
+		this.pre = this.shadowRoot.querySelector('pre');
+		const slot = this.shadowRoot.querySelector('slot');
+		const result = this.resetSpaces(this);
+
+		if (this.inline) this.pre.classList.add('inline');
+		this.textContent = result;
+
+		slot.addEventListener('slotchange', event => {
+			if (!this.#firstPaint) {
+				this.textContent = this.resetSpaces(this);
+			}
+			this.#firstPaint = false;
 		});
-
-		this.addCss();
-		this.wrapCode(this, content);
-		this.observer.observe(this, this.mutationConfig);
-	}
-
-	disconnectedCallback () {
-		this.observer.disconnect();
 	}
 
 	attributeChangedCallback (attr, oldval, newval) {
 		this[attr] = newval;
-	}
-
-	observe(mutationList, observer) {
-		this.observer.disconnect();
-		const content = this.resetSpaces(this);
-		this.wrapCode(this, content);
-		this.observer.observe(this, this.mutationConfig);
-	}
-
-	wrapCode(container, content) {
-		const pre = document.createElement('pre');
-		const code = document.createElement('code');
-		if (this.inline) pre.classList.add('inline');
-		pre.append(code);
-		code.textContent = content;
-		container.replaceChildren(pre);
 	}
 
 	resetSpaces (container) {
@@ -84,45 +83,19 @@ export default class WijitCode extends HTMLElement {
 		return lines.map (line => line.replace(regex, '')).join("\n");
 	}
 
-	addCss () {
-		const id = 'wijit-code-css';
-		const style = document.createElement('style');
-		const css = `
-			wijit-code {
-				--tabsize: ${this.tabsize};
-				display: inline-block;
-				vertical-align: middle;
-			}
-
-			wijit-code pre { tab-size: var(--tabsize); }
-
-			wijit-code pre.inline {
-				margin: 0;
-				white-space: nowrap;
-			}
-		`;
-
-		if (!document.head.querySelector(`#${id}`)) {
-			style.id = id;
-			style.textContent = css;
-			document.head.append(style);
-		}
-	}
-
 	get inline () { return this.#inline; }
 	set inline (value) {
-		const pre = this.querySelector('pre');
 		switch (value) {
 		case '':
 		case 'true':
 		case true:
 			value = true;
-			if (pre) this.pre.classList.add('inline');
+			if (this.pre) this.pre.classList.add('inline');
 			break;
 		default:
 			value = false;
 			this.removeAttribute('inline');
-			if (pre) this.pre.classList.remove('inline');
+			if (this.pre) this.pre.classList.remove('inline');
 			break;
 		}
 		this.#inline = value;
